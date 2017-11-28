9cca7b820a4ff09089b3decadc88b6d47428eb51
xia <343486024@qq.com>
2017-11-28 16:12:39 +0800

    Add elementUI

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
+
+Vue.use(elementUI)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })

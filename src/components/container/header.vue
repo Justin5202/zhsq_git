88b71a46f432a10e832c489b6a722ac7f12f5e01
xia <343486024@qq.com>
2017-11-28 16:12:29 +0800

    Update header

---
+++ src/components/container/header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="header">
+      <el-row>
+        <el-col :span="12">
+          <div id="logo">
+            <p>logo</p>
+          </div>
+        </el-col>
+        <el-col :span="8">
+            <v-menu/>
+        </el-col>
+        <el-col :span="4">
+          <div id="user">
+            <p>user</p>
+          </div>
+        </el-col>
+      </el-row>
+  </div>
+</template>
+
+<script>
+import vMenu from '../content/menu'
+export default {
+  name: 'header',
+  components: {
+    vMenu
+  }
+}
+</script>
+<style lang="scss">
+#header{
+  background-color: rgb(84, 92, 100)
+}
+</style>
---
+++ src/components/container/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <el-row>
+          <el-col :span="24">
+              <v-header/>
+          </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+import vHeader from './header'
+export default {
+  name: 'container',
+  components: {
+    vHeader
+  }
+}
+</script>
+
+<style>
+
+</style>

6ec4ba6a104d18f0f9ad10e84b08002a3d025993
xia <343486024@qq.com>
2017-11-30 11:11:28 +0800

    New d2cmap component

---
+++ src/components/common/d2cmap.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="map" class="wh100"></div>
+</template>
+<script>
+export default {
+  name: 'd2cmap',
+  props: ['option'],
+  data () {
+    return {
+      map: null
+    }
+  },
+  mounted () {
+    this.initMap()
+    window.addEventListener('resize', this.resize)
+  },
+  beforeDestroy () {
+    this.map.remove()
+  },
+  methods: {
+    initMap () {
+      this.map = new window.d2c.map(this.option)
+    },
+    resize () {
+      this.map.resize()
+    }
+  }
+}
+</script>
+
+<style>
+  #map canvas{
+    top: 0;
+    left: 0;
+  }
+</style>

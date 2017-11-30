75f094809316d5730979271db078ed7a11fbe50f
xia <343486024@qq.com>
2017-11-30 11:16:06 +0800

    New page for test

---
+++ src/components/page/testPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="testPage">
+      <h1>
+          {{msg}}
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'testPage',
+  data () {
+    return {
+      msg: ' TEST PAGE'
+    }
+  }
+}
+</script>
+
+<style>
+#testPage{
+  height: 400px;
+  width: 400px;
+  background-color: red
+}
+</style>

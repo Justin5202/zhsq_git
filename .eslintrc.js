e110d326cda23dde59f6991f21db98005d70f0e7
xia <343486024@qq.com>
2017-11-30 11:06:47 +0800

    Update project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,8 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // allows new operators to be called with lowercase-started or uppercase-started functions
+    'new-cap': 0
   }
 }

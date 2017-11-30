7d54639f7a5bdafed82d2fe9a2566d36078cea48
xia <343486024@qq.com>
2017-11-30 11:11:55 +0800

    New map page content

---
+++ src/components/content/map_test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="map_test" :style="style">
+    <v-map :option='option'/>
+  </div>
+</template>
+
+<script>
+import vMap from '../common/d2cmap'
+import mWindow from '../../util/window'
+import mapOption from '../../settings/option'
+export default {
+  name: 'test_map',
+  components: {
+    vMap
+  },
+  mixins: [mWindow],
+  data () {
+    return {
+      option: mapOption()
+    }
+  },
+  computed: {
+    style () {
+      return {
+        height: this.window.height - 61 + 'px',
+        width: this.window.width + 'px'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+

66d50360513880cf54e59e325a4c8ecf6fef3de7
xia <343486024@qq.com>
2017-11-30 11:10:47 +0800

    Update layout css

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-body{
+body {
   margin: 0 0;
+}
+.wh100{
+  height: 100%;
+  width: 100%;
+}
+
+.router{
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import elementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css' // elementUI css
+import './assets/d2c/d2c.css' // d2c css
 Vue.config.productionTip = false
 
 Vue.use(elementUI)

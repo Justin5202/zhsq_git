5e80a2eed405e162c4d23c25eda2f79d48748b8a
xia <343486024@qq.com>
2017-12-06 21:36:17 +0800

    Complete basic router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import {route as toolbar} from '@/settings/toolbar'
 
 Vue.use(Router)
 
 // 定义组件
 const container = r => require(['@/components/container/index'], r)
 const testPage = r => require(['@/components/page/testPage'], r)
+const mapTest = r => require(['@/components/page/map_test'], r)
 
 export default new Router({
   routes: [
@@ -16,14 +18,19 @@ export default new Router({
         {
           path: '',
           name: 'index',
-          redirect: 'home'
+          redirect: 'map_test'
         }, {
           path: '/test_page',
           name: 'testPage',
           component: testPage
         }, {
-          path: '/home',
-          name: 'home'
+          path: '/map_test',
+          name: 'mapTest',
+          meta: {keepAlive: true},
+          component: mapTest,
+          children: [
+            ...toolbar
+          ]
         }
       ]
     }

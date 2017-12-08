a700883c376c36afd9c7da9f822690afa553d9e5
xia <343486024@qq.com>
2017-12-08 16:54:12 +0800

    Import shenzhen style

--- src/settings/option.js
+++ src/settings/option.js
@@ -3,11 +3,12 @@
  * @Author: xia
  * @Date: 2017-11-30 09:34:45
  * @Last Modified by: xia
- * @Last Modified time: 2017-12-07 15:44:33
+ * @Last Modified time: 2017-12-07 21:58:41
  */
 import chongqingLocal from '../../static/style/chongqing_local'
 import chongqing from '../../static/style/chongqing'
 import guiyang_local from '../../static/style/guiyang_local'
+import shenzhen from '../../static/style/shenzhen'
 const cq1 = {
   container: 'map',
   center: [106.994999, 30.17728484178069],
@@ -30,9 +31,17 @@ const gy1 = {
   style: guiyang_local,
   zoom: 8,
   maxZoom: 18
+
+}
+const sz1 = {
+  container: 'map',
+  center: [113.924103, 22.782512000000001],
+  style: shenzhen,
+  zoom: 11,
+  maxZoom: 18
 }
 
 export default (option) => ({
-  ...gy1,
+  ...sz1,
   ...option
 })

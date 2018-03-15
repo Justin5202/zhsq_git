4b2c839c26561babb7ac51e48b825e4083a31cf9
wangsiyu <jingcookie@foxmail.com>
2018-03-15 16:56:46 +0800

    调试 maphelper  setFilterByCodeArrayAndAreacodeArray

--- src/util/mapHelper.js
+++ src/util/mapHelper.js
@@ -566,6 +566,7 @@ const setFilterByCodeArrayAndAreacodeArray = function (_codeArray, _areacodeArra
     // 记录 目录和区域 在styledata 事件触发时 过滤
     codeArray = _codeArray;
     areacodeArray = _areacodeArray;
+
 };
 
 /**

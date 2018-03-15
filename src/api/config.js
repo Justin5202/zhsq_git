55f78ec4dcb8678d8af100308928e950c0fb4b89
web-Liling <386659643@qq.com>
2018-03-15 19:46:31 +0800

    merge

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
 //通用参数
 export const commonParams = {
-  code: '0ea0fffce6d83c7946fb626f6e3cce2b',
-  e: '624cb7000edfc4c53aaf616a7b1f6844',
-  os: 'ios_ipad'
+    code: '0ea0fffce6d83c7946fb626f6e3cce2b',
+    e: '624cb7000edfc4c53aaf616a7b1f6844',
+    os: 'ios_ipad'
 }
 
 // 域名

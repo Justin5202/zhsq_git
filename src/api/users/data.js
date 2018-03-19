f7af9b22213af832934299cdb736c238eb0e93f4
zengwenquan <724708892@qq.com>
2018-03-19 16:18:36 +0800

    添加获取反馈历史方法

--- src/api/users/data.js
+++ src/api/users/data.js
@@ -3,7 +3,7 @@ import qs from 'qs'
 import { commonParams, url } from './config'
 import MD5 from 'crypto-js/md5'
 
-export function login(username, password, code) {
+export function login(username, password, code) { // 登录
 
   let data = Object.assign({}, commonParams, {
     method: 'login',
@@ -15,19 +15,18 @@ export function login(username, password, code) {
   })
 }
 
-export function feedback(text) {
+export function feedback(text) { // 意见反馈
   let data = Object.assign({}, commonParams, {
     method: 'guestbook',
     suggest: text,
     type: '0'
   })
-  console.log(data)
   return axios.post(url, qs.stringify(data)).then(res => {
     return Promise.resolve(res.data)
   })
 }
 
-export function fetchData(time) {
+export function fetchData(time) { // 获取系统消息
   let data = Object.assign({}, commonParams, {
     method: 'getPushData',
     uptime: time
@@ -39,5 +38,21 @@ export function fetchData(time) {
   params = params.substring(0, params.length - 1)
   return axios.get(`${url}?${params}`).then(res => {
       return Promise.resolve(res.data)
+  })
+}
+
+export function getSuggestList(start) { // 获取反馈历史
+  let data = Object.assign({}, commonParams, {
+    method: 'getSuggestList',
+    start: start,
+    rows: 10
+  })
+  let params = ''
+  for (let key in data) {
+    params += `${key}=${data[key]}&`
+  }
+  params = params.substring(0, params.length - 1)
+  return axios.get(`${url}?${params}`).then(res => {
+    return Promise.resolve(res.data)
   })
 }

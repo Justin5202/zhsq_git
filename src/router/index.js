7a8444ff54d8850e26a20b36b27c449f25e6d4af
zengwenquan <724708892@qq.com>
2018-03-21 14:09:52 +0800

    删除个人中心的路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -37,10 +37,6 @@ export default new Router({
           children: [
             ...toolbar
           ]
-        }, {
-          path: '/userCenter',
-          name:'userCenter',
-          component: userCenter,
         }
       ]
     },

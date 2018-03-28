f23784b6fcf30caa7900ed246e2aca5a65bf6b05
zengwenquan <724708892@qq.com>
2018-03-28 09:57:20 +0800

    修改loading图标

--- src/util/http.js
+++ src/util/http.js
@@ -19,7 +19,11 @@ axios.interceptors.request.use(function (config) {
 
 // 拦截响应
 axios.interceptors.response.use(function (response) {
-  let loading = Loading.service({})
+  let loading = Loading.service({
+    fullscreen: true,
+    background: 'rgba(0, 0, 0, .6)',
+    spinner: 'icon-loading'
+  })
   loading.close()
   if (response.data.code == '-3') {
     router.replace('/login')

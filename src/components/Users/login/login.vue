dcc96917b02c87da31331c71e126dd71b25edfdc
zengwenquan <724708892@qq.com>
2018-03-29 15:48:43 +0800

    修改登录样式

--- src/components/Users/login/login.vue
+++ src/components/Users/login/login.vue
@@ -13,21 +13,22 @@
             <input 
             class="el-input" 
             type="password" 
-            v-model="form.password"
+            v-model="form.password" 
             @keyup.enter="_login(form.username, form.password)">
           </div>
           <div class="btn-box">
-            <el-button type="primary" @click="_login(form.username, form.password)">登录</el-button>
+            <el-button type="primary" @click="_login(form.username, form.password)">登&nbsp;&nbsp;&nbsp;录</el-button>
             <router-link to="register" tag="span" class="link">
-              <el-button type="primary">注册</el-button>
+              <el-button type="primary">注&nbsp;&nbsp;&nbsp;册</el-button>
             </router-link>
           </div>
         </form>
       </div>
-    </div>
-    <div class="info">
-      <p>申请用户名请联系地理信息中心</p>
-      <p>联系人:赵翔宇 电话:67033391</p>
+      <div class="info">
+        <p>申请用户名请联系地理信息中心</p>
+        <p>联系人:赵翔宇 电话:023-67033881</p>
+        <p>邮箱:57012855@qq.com</p>
+      </div>
     </div>
   </div>
 </template>
@@ -86,78 +87,77 @@ export default {
 </script>
 <style lang="scss" scoped>
   .login {
-    position: relative;
     width: 100%;
-    min-height: 768px;
     height: 100%;
     background: url('../../../assets/images/Login/logbg@2x.png') no-repeat;
     background-size: 100% 100%;
     color: #fff;
+    position: fixed;
     .wrap {
       width: 1200px;
-      position: absolute;
-      top: 200px;
-      left: 50%;
-      transform: translateX(-50%);
+      height: 100%;
+      // min-height: 1000px;
+      margin: 0 auto;
+      position: relative;
       h1 {
-        font-size: 48px;
+        padding-top: 10%;
+        font-size: 45px;
         margin-bottom: 20px;
         letter-spacing: 3px;
       }
       .form {
-        position: absolute;
-        width: 512px;
-        top: 184px;
-        right: 0;
-        padding-top: 40px;
-        .el-form-item {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          margin-bottom: 44px;
-          label {
-            width: 86px;
-            font-size: 24px;
-            letter-spacing: 1px;
-            margin-right: 24px;
+        width: 100%;
+        position: relative;
+        .el-form {
+          width: 512px;
+          position: absolute;
+          right: 0;
+          top: 120px;
+          .el-form-item {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 44px;
+            label {
+              font-size: 24px;
+              width: 86px;
+              margin-right: 24px;
+            }
+            .el-input {
+              height: 56px;
+              width: 404px;
+              border: 1px solid lightgrey;
+              border-left: 0;
+              outline: none;
+              padding: 0 5px;
+              box-sizing: border-box;
+            }
           }
-          .el-input {
-            height: 56px;
-            width: 404px;
-            border: 1px solid lightgrey;
-            border-left: 0;
-            outline: none;
-            font-size: 18px;
-            padding: 0 5px;
-            box-sizing: border-box;
-          }
-        }
-        .btn-box {
-          width: 100%;
-          display: flex;
-          justify-content: space-between;
-          padding-top: 24px;
-          .el-button {
-            height: 56px;
-            width: 232px;
-            font-size: 16px;
-            font-size: 24px;
-            letter-spacing: 15px;
-            border: 0;
-            border-radius: 0;
-            background-color: #002a4f;
+          .btn-box {
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            padding-top: 24px;
+            .el-button {
+              width: 232px;
+              height: 56px;
+              font-size: 24px;
+              border: 0;
+              border-radius: 0;
+              background-color: #002a4f;
+            }
           }
         }
       }
-    }
-    .info {
-      position: absolute;
-      left:50%;
-      bottom: 5%;
-      transform: translateX(-50%);
-      p {
-        letter-spacing: 1px;
-        padding-bottom: 5px;
+      .info {
+        position: absolute;
+        left:50%;
+        bottom: 10%;
+        transform: translateX(-50%);
+        p {
+          letter-spacing: 1px;
+          padding-bottom: 5px;
+        }
       }
     }
   }
